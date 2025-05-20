0af80d9a63b3f04114581893598dfc0d4fa5ff0a
Ana Luiza <analu.tibi@outlook.com>
2025-05-20 11:38:22 -0300

    Criando JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const loginForm=document.getElementById('form-login');
+const mensae=document.getElementById('message');
+const video=document.querySelector('.video-conatiner');
+})
